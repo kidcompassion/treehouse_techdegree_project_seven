de8156289b908435e6ddb24859d6510e76a7e444
kidcompassion <sallypoulsen@gmail.com>
2020-03-10 14:23:38 -0600

    Updates

---
+++ precache-manifest.8f1504c657cef9f6b2a4963608d834a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2525b7145733b252014b27a9578fed16",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/index.html"
+  },
+  {
+    "revision": "23b8963666f38f466793",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/static/css/main.e37efecb.chunk.css"
+  },
+  {
+    "revision": "c5a940e00b09bfb9387b",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/static/js/2.d49b5157.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/static/js/2.d49b5157.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23b8963666f38f466793",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/static/js/main.833345d8.chunk.js"
+  },
+  {
+    "revision": "ad04ea0eef832b420000",
+    "url": "/kidcompassion/treehouse_techdegree_project_seven/static/js/runtime-main.05c47ba4.js"
+  }
+]);
